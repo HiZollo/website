600283da577d754b533ee384c112a6d35016c297
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-03-15 11:13:14 +0800

    media query mathter

--- components/main.tsx
+++ components/main.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next'
 import Link from 'next/link';
 import wordmark from '../public/wordmark.png';
-import Image from 'next/image'
+import wordmarkLarge from '../public/wordmark-large.png';
+import Image from 'next/image';
 
 const Layout: NextPage = ({ children }) => {
   return (
     <>
       <header>
+        <div id="wordmark-large">
+          <Link href="/">
+            <a><Image src={wordmarkLarge} alt="Junior HiZollo" /></a>
+          </Link>
+        </div>
         <div id="wordmark">
           <Link href="/">
             <a><Image src={wordmark} alt="Junior HiZollo" /></a>
