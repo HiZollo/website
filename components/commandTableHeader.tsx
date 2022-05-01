1ba8e3c76b8ba22b622ea7a8423626756280e8a8
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-05-01 13:54:38 +0800

    fix: fix an error that will pop up since react 18

--- components/commandTableHeader.tsx
+++ components/commandTableHeader.tsx
@@ -2,22 +2,26 @@ import styles from '../styles/Commands.module.css';
 
 function TableHeader () {
   return (
-    <tr>
-      <td className={styles['helplist-header']}><strong>指令</strong></td>
-      <td className={styles['helplist-header']}><strong>替代指令</strong></td>
-      <td className={styles['helplist-header']}><strong>指令說明</strong></td>
-      <td className={styles['helplist-header']}><strong>範例</strong></td>
-    </tr>
-  )
+    <thead>
+      <tr>
+        <td className={styles['helplist-header']}><strong>指令</strong></td>
+        <td className={styles['helplist-header']}><strong>替代指令</strong></td>
+        <td className={styles['helplist-header']}><strong>指令說明</strong></td>
+        <td className={styles['helplist-header']}><strong>範例</strong></td>
+      </tr>
+    </thead>
+  );
 }
 
 function SlashTableHeader () {
   return (
-    <tr>
-      <td className={styles['helplist-header']}><strong>指令</strong></td>
-      <td className={styles['helplist-header']}><strong>指令說明</strong></td>
-    </tr>
-  )
+    <thead>
+      <tr>
+        <td className={styles['helplist-header']}><strong>指令</strong></td>
+        <td className={styles['helplist-header']}><strong>指令說明</strong></td>
+      </tr>
+    </thead>
+  );
 }
 
 export { TableHeader, SlashTableHeader };
