5a449857a3b967cffdaf2eb8514f5af47d062706
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-05-01 20:27:52 +0800

    feature: introduce menu to header

--- components/main.tsx
+++ components/main.tsx
@@ -1,11 +1,23 @@
 import type { NextPage } from 'next';
-import { ReactElement } from 'react';
+import React, { useState, ReactElement } from 'react';
 import Link from 'next/link';
 import wordmark from '../public/wordmark.png';
 import wordmarkLarge from '../public/wordmark-large.png';
 import Image from 'next/image';
 
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
 const Layout: NextPage<{ children: ReactElement }> = ({ children }) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <header>
@@ -20,18 +32,34 @@ const Layout: NextPage<{ children: ReactElement }> = ({ children }) => {
           </Link>
         </div>
         <div id="blank" />
-        <Link href="/commands">
-          <a>指令列表</a>
-        </Link>
         <Link href="/tutorials">
           <a>使用教學</a>
         </Link>
-        <Link href="/changelog">
-          <a>更新日誌</a>
-        </Link>
-        <Link href="/developers">
-          <a>開發團隊</a>
-        </Link>
+        <HeaderMenu id="infos" text="更多資訊">
+          <Link href="/commands" passHref>
+            <MenuItem onClick={handleClose}>指令列表</MenuItem>
+          </Link>
+          <Link href="/changelog" passHref>
+            <MenuItem onClick={handleClose}>更新日誌</MenuItem>
+          </Link>
+          <Link href="/developers" passHref>
+            <MenuItem onClick={handleClose}>開發團隊</MenuItem>
+          </Link>
+        </HeaderMenu>
+        <HeaderMenu id="links" text="相關連結">
+          <Link href="/invite" passHref>
+            <MenuItem onClick={handleClose}>邀請 HiZollo</MenuItem>
+          </Link>
+          <Link href="/server" passHref>
+            <MenuItem onClick={handleClose}>支援伺服器</MenuItem>
+          </Link>
+          <Link href="/dst" passHref>
+            <MenuItem onClick={handleClose}>DST 頁面</MenuItem>
+          </Link>
+          <Link href="/repo" passHref>
+            <MenuItem onClick={handleClose}>網站原始碼</MenuItem>
+          </Link>
+        </HeaderMenu>
         <Link href="/tos">
           <a>用戶條款</a>
         </Link>
@@ -50,4 +78,43 @@ export default Layout;
 
 function BlankDiv() {
   return <div style={{ height: '100px' }}></div>;
+}
+
+interface HeaderMenuProps {
+  children?: React.ReactNode,
+  id: string,
+  text: string
+}
+
+function HeaderMenu(props: HeaderMenuProps) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <button
+        aria-controls={open ? props.id : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      ><a>{props.text}</a></button>
+      <Menu
+        id={props.id}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        {props.children}
+      </Menu>
+    </>
+  );
 }
