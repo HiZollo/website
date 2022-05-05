eca3065f51e63735415fadeecaa34fc891432fd1
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-05-06 01:46:48 +0800

    chore: add tmp blog page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import { readDirSync } from 'fs';
+
+const Blog: NextPage = () => {
+  return (
+    <>
+      <h1>開發日誌</h1>
+      這裡是 HiZollo 的開發日誌，當開發者很閒時可能就會寫一篇丟在這裡。不保證更新頻率。
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    </>
+  );
+}
+
+export default Blog;
