b5e1f6d890397116863f533ba6841699bd8edff1
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-05-06 01:53:42 +0800

    chore: add header to tutorial page

--- pages/tutorials/index.tsx
+++ pages/tutorials/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import Header from '../../components/head';
 import TutorialNav from '../../components/tutorialNav';
 
 const Tutorials: NextPage = () => {
   return (
     <>
+      <Header title="使用教學" description="這裡是 HiZollo 的使用教學，如果你不知道怎麼使用 HiZollo，或是你想發揮 HiZollo 全部的功能，你一定要看這裡。" />
       <h1>使用教學</h1>
       這裡是 HiZollo 的使用教學，如果你不知道怎麼使用 HiZollo，或是你想發揮 HiZollo 全部的功能，你一定要看這裡。
       <TutorialNav />
