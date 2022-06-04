7fd86af103d1464b421204dbecb0d075aa4dcc96
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-06-04 12:52:52 +0800

    chore: change the site source link to the repo

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     'inviteUrl': 'https://discord.com/oauth2/authorize?client_id=584677291318312963&permissions=1636382010871&scope=bot+applications.commands',
     'serverUrl': 'https://discord.com/invite/xUXTrYG2MZ',
     'dstUrl': 'https://discordservers.tw/bots/584677291318312963',
-    'repoUrl': 'https://github.com'
+    'repoUrl': 'https://github.com/HiZollo/website'
   },
   async redirects() {
     return [
