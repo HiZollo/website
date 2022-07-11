8055929874c460c119d1015990b28c9e52bd1a22
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-07-11 21:39:53 +0800

    chore: add loading progress animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import '../styles/globals.css'
 import Layout from '../components/main';
+import NextNProgress from "nextjs-progressbar";
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <NextNProgress
+        color="#A7B5D7"
+        height={3}
+        showOnShallow={true}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
